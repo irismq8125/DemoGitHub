730c37512322c58ca4cfbbf99230ea0513b71de4
hmquy <mauquy123@gmail.com>
2023-03-19 09:45:15 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //nhap vao so nguyen a
+            Console.Write("Nhap vao so nguyen a = ");
+            int a = int.Parse(Console.ReadLine());
+            //nhap vao so nguyen b
+            Console.Write("Nhap vao so nguyen b = ");
+            int b = int.Parse(Console.ReadLine());
+
+            //hien thi ket qua
+            Console.WriteLine("<-------------------->");
+            Console.WriteLine("So nguyen a vua nhap la: a = {0}",a);
+            Console.WriteLine("So nguyen b vua nhap la: b = {0}",b);
+            Console.WriteLine("<-------------------->");
+            Console.WriteLine("Bam phim bat ky de ket thuc!");
+            Console.ReadKey();
+        }
+    }
+}
