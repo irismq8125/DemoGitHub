2b5e1f02a81ad689e256e39a36f0bb62695419f6
hmquy <mauquy123@gmail.com>
2023-03-19 09:47:18 +0700

    add thuong

--- Program.cs
+++ Program.cs
@@ -17,10 +17,14 @@ namespace DemoGitHub
             Console.Write("Nhap vao so nguyen b = ");
             int b = int.Parse(Console.ReadLine());
 
+            float c = (a * 1.0f) / b;
+
             //hien thi ket qua
             Console.WriteLine("<-------------------->");
             Console.WriteLine("So nguyen a vua nhap la: a = {0}",a);
             Console.WriteLine("So nguyen b vua nhap la: b = {0}",b);
+
+            Console.WriteLine("Thuong cua a va b = {0}", c);
             Console.WriteLine("<-------------------->");
             Console.WriteLine("Bam phim bat ky de ket thuc!");
             Console.ReadKey();
